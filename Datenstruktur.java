34724e364b2da06b151a3bb703b5562b5e91c071
Roland Daidone <r.daidone@t-online.de>
2018-05-29 18:59:04 +0200

    Gemeinsames Projekt angelegt

---
+++ Datenstruktur.java
@@ -0,0 +1,11 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Datenstruktur.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Datenstruktur
+{
+//Kommt noch
+}
